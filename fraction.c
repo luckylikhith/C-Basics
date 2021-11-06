bbdf283deced07b7a79afbf8e6f8b943dc4f10c7
luckylikhith <92074688+luckylikhith@users.noreply.github.com>
2021-11-06 10:49:17 +0530

    Add files via upload

--- fraction.c
+++ fraction.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 void main()
 {
-	int Q,A;
+	int Q,A,B,C;
 	Q=20;
 	A=Q*(Q+1)/2;
+	B=Q*(Q+1);
+	C=Q*Q;
 
-	printf("Sum of first %d natural numbers is %d", Q,A );
+	printf("Sum of first %d natural numbers \t = %d", Q,A );
+	printf("\n\nsum of first %d even natural numbers \t = %d", Q,B);
+	printf("\n\nsum od first %d odd natural numbers \t = %d", Q,C);
 }
